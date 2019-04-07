cc076f6355111406ac48f79f27d499c7043c74b7
jehfkemsy <jehf.denezaire@gmail.com>
2019-04-07 06:18:13 -0400

    added ignore websocket warnings

--- client/App.js
+++ client/App.js
@@ -7,6 +7,14 @@ import Home from './src/scenes/home'
 import Login from './src/scenes/auth'
 import SignIn from './src/scenes/auth/signin'
 
+
+console.ignoredYellowBox = ['Remote debugger'];
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings([
+    'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
+
+
 let SERVER_URL = "http://00d4e500.ngrok.io";
 const socket = io.connect(SERVER_URL);
 
